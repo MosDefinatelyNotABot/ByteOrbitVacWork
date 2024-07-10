176a75e5947ab3dc1b76289ddb3c41815f8fbdc3
Min Kim <rlaals31@gmail.com>
2024-07-10 14:26:13 +0200

    Update database.py
    verify() still needs to be updated but connecting to & creating DB work as well as adding new table (for faces) & adding new faces into the existing table.

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
+# postgreSQL database to store faces (name & img_encoding)
+
+
 import psycopg2 # make sure to pip install psycopg2 for postgreSQL & downloaded postgreSQL on your machine
-                    # also make sure your postgreSQL is running if running locally
+                # also make sure your postgreSQL is running if running locally
 from psycopg2 import sql
+from pgvector.psycopg2 import register_vector   # pip install pgvector
 import pickle 
-import numpy as np
+import numpy as np  # pip instll numpy
 
 
 
 class vectorDB:
 
-    # myDB = vectorDB()
-    def __init__(self, user : str, password : str, host = 'localhost'): # if host not given, uses localhost
+    # myDB = vectorDB('user', password, database, host)
+    def __init__(self, user : str, password : str, database : 'postgres', host = 'localhost'): # if host not given, uses localhost
         self.user = user
         self.password = password    # probably not the safest approach to get pw ??
-        self.dbName = "postgres"
+        self.database = database
         self.host = host
 
         '''
@@ -20,46 +24,48 @@ class vectorDB:
                                 host='localhost', database='FaceDetect')
         '''
 
-        self.conn = psycopg2.connect(user=self.user, password=self.password,
-                                host=self.host, dbName=self.dbName)
+        self.conn = psycopg2.connect(user=self.user, password=self.password, database = database, host=self.host)
         
         print("Connection established")
 
+
         self.conn.set_session(autocommit=True)
     
-
-    # myDB.createDB("faceDetection")
-    def createDB(self, dbName : str):
         cursor = self.conn.cursor()
 
-        cursor.execute(sql.SQL("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s"), [dbName]) # checks if db with this name exists
+        cursor.execute(sql.SQL("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s"), [database]) # checks if db with this name exists
 
         exists = cursor.fetchone()
         
         if not exists:
-            cursor.execute(sql.SQL("CREATE DATABASE %s"), [dbName])  # ensures no duplicate DB is created
+            cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(database)))  # ensures no duplicate DB is created
+            print("Database successfully created!")
+        else:
+            print("Successfully connected to database!")
+
+        cursor.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+        # CREATE EXTENSION IF NOT EXISTS vector ## still need to work on this
+        # register_vector(self.conn) 
         
-        print("Database successfully created!")
+        
 
     
-    # myDB.createTable("Faces")
+    # myDB.createTable('Faces')
     def createTable(self, tableName : str):
 
         cursor = self.conn.cursor()
+    
+        cursor.execute(sql.SQL("DROP TABLE IF EXISTS {}").format(sql.Identifier(tableName)))    # deletes table if exists
 
-        cursor.execute(
+        cursor.execute(sql.SQL(
             """
-            CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-            DROP TABLE IF EXISTS {%s};
-
-            CREATE TABLE IF NOT EXISTS faces (
-                id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-                name VARCHAR(255) NOT NULL,
-                encoding BYTEA NOT NULL
-            )
+            CREATE TABLE {}(
+            id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+            name VARCHAR(255) NOT NULL,
+            encoding BYTEA NOT NULL)
             """
-        )[tableName]
+        ).format(sql.Identifier(tableName)))
+
 
         print("Table successfully created!")
 
@@ -67,37 +73,36 @@ class vectorDB:
 
 
     # myDB.addFaces('andrew', img_to_encoding("images/andrew.jpg", FRmodel))
-    def addFaces(self, name : str, encoding : np):  # otherwise take image_path : str rather than encoding
+    def addFaces(self, tableName : str, name : str, encoding : np):  # otherwise take image_path : str rather than encoding
     
         cursor = self.conn.cursor()
 
-        add_faces = ("INSERT INTO FACES "
+        addFace = ("INSERT INTO {} "
                     "(name, encoding) "
-                    "VALUES (%s, %s)")
+                    "VALUES (%s, %s)").format(tableName)
         
-        pickled_encoding = pickle.dumps(encoding) # dumps() serialises an object
+        pickledEncoding = pickle.dumps(encoding) # dumps() serialises an object
         
-        data_faces = (name, pickled_encoding)
+        dataFace = (name, pickledEncoding)
 
-        cursor.execute(add_faces, data_faces)
+        cursor.execute(addFace, dataFace)
 
-        print("Added faces to db")
+        print("Successfully added faces to db!")
 
         cursor.close()
 
 
     # myDB.verify("images/andrew.jpg", "andrew", FRmodel)
-    def verify(self, image_path, identity, model): # erased 'database' from prev version
+    def verify(self, tableName : str, image_path : str, identity : str, model): # erased 'database' from prev version
         
         cursor = self.conn.cursor()
-
         
         encoding = img_to_encoding(image_path, model)
 
-        query = ("SELECT encoding FROM faces WHERE name='%s'") % (identity)
-        cursor.execute(query)
+        query = ("SELECT encoding FROM {} WHERE name='%s'").format(tableName)
+        
+        cursor.execute(query, identity)
     
-
         unpickled_encoding = pickle.loads(cursor.fetchone()[0]) # back to numpy
     
 
@@ -118,6 +123,16 @@ class vectorDB:
     # myDB.close_conn()
     def close_conn(self):
         self.conn.close()
+        print("Successfully disconnected from db!")
+
+
+myDB = vectorDB('postgres', '2518', 'testDB', 'localhost')
+myDB.createTable('faces')
+myDB.addFaces('faces', 'andrew', 'img_to_encoding("images/andrew.jpg", FRmodel')
+myDB.addFaces('faces', 'danielle', 'img_to_encoding("images/danielle.png", FRmodel')
+myDB.addFaces('faces', 'kian', 'img_to_encoding("images/kian.jpg", FRmodel')
+myDB.verify('faces', 'images/andrew.jpg', 'danielle', FRmodel)  
+
 
 
 
